a6318c9556abab5f032d7a66a7ac5257154cc945
LostSoulfly <9120519+LostSoulfly@users.noreply.github.com>
2019-01-19 02:28:10 -0500

    Add Class Ignore Hashset

--- Bifrost/Logging/EventSink.cs
+++ Bifrost/Logging/EventSink.cs
@@ -1,10 +1,13 @@
-﻿using Serilog.Core;
+﻿using Serilog;
+using Serilog.Core;
 using Serilog.Events;
 using Serilog.Formatting;
 using Serilog.Formatting.Display;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Text;
 using static Bifrost.Delegates;
 
 namespace Bifrost
@@ -19,20 +22,44 @@ namespace Bifrost
         private readonly static IFormatProvider _formatProvider = null;
 
         private ITextFormatter _textFormatter = new MessageTemplateTextFormatter("{Timestamp:HH:mm:ss.fff} [{Level}] {Message}{Exception}", _formatProvider);
+        private ITextFormatter _textFormatterClass = new MessageTemplateTextFormatter("{Timestamp:HH:mm:ss.fff} [{Level}] <{Class}> {Message}{Exception}", _formatProvider);
 
         public static event LogMessage OnLogEvent;
+
+        public HashSet<string> ignoreHashSet = new HashSet<string>();
 
         public void Emit(LogEvent logEvent)
         {
             if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
-            var renderSpace = new StringWriter();
-            _textFormatter.Format(logEvent, renderSpace);
 
+            string logMessage = null;
+
+            if (logEvent.Properties.ContainsKey("Class"))
+            {
+                LogEventPropertyValue property;
+                if (logEvent.Properties.TryGetValue("Class", out property))
+                {
+
+                    string ignoreClass = property.ToString();
+                    if (ignoreHashSet.Contains(ignoreClass))
+                        return;
+
+                    var render = new StringWriter();
+                    _textFormatterClass.Format(logEvent, render);
+                    logMessage = render.ToString();
+                }
+            }
+            else
+            {
+                var renderSpace = new StringWriter();
+                _textFormatter.Format(logEvent, renderSpace);
+                logMessage = renderSpace.ToString();
+            }
             //Raise the event on the delegate's thread, should be UI.
             //Necessary otherwise deadlocks ensue and handshake failure due to cross-thread calls..
-            RaiseEventOnUIThread(OnLogEvent, renderSpace.ToString());
+            RaiseEventOnUIThread(OnLogEvent, logMessage);
         }
-
+        
         private void RaiseEventOnUIThread(Delegate theEvent, string args)
         {
             if (theEvent == null)
@@ -47,7 +74,11 @@ namespace Bifrost
                 }
                 else
                 {
-                    syncer.BeginInvoke(d, new object[] { args });
+                    try
+                    {
+                        syncer.BeginInvoke(d, new object[] { args });
+                    }
+                    catch { }
                 }
             }
         }
